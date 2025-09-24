b7a6fa2d64556f999de8544b09030b6d8d4a82e2
julio souza <julio_1997_@hotmail.com>
2025-09-24 15:37:24 -0300

    Criado o projeto

---
+++ src/main/java/com/dragonball/dbz_battle_simulator/DbzBattleSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.dragonball.dbz_battle_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbzBattleSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbzBattleSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dragonball/dbz_battle_simulator/DbzBattleSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragonball.dbz_battle_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbzBattleSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
